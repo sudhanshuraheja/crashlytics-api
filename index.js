d9e4e4fcf74a4c47cd1609ede8fd559d8c0bb4f7
Sudhanshu Raheja <sudhanshu.raheja@go-jek.com>
2017-01-29 10:57:28 +0530

    Added commander, moved from cli params to config for credentials, removed fabric.json from commits, removed cli

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
 "use strict"
 
-// Read CLI to figure out the login and password
-let user = require('./cli')()
+// Start the CLI Manager
+const program = require('commander')
+
+// Read Package.json and Config
+const pkg = require('./package.json')
+const conf = require('./fabric.json')
+
+console.log(conf.user)
+
+program
+  .version(pkg.version)
+  .option('-h, --help', 'Help')
+  .parse(process.argv)
 
 // Load up the API
 let API = require("./api")
 
 // TODO : Move to promises, this is sad
-let api = new API(user)
+let api = new API(conf.user)
 api.load(() => {
   api.configData(() => {
     api.login(() => {
